c222ed9fa8cbb20058e1bf146b3a9cafd477fe18
shahrukhnasir <shahrukhnasir0108@gmail.com>
2024-08-29 16:12:09 +0500

    update responsive web

--- inc/header.php
+++ inc/header.php
@@ -78,7 +78,7 @@ $isHomePage = ($activePage == 'index');
                 <a href="tel:+923333002133">
                     <div class="" style="width: 30px;">
 
-                        <img src="./assets/images/icons/whatsapp.png" class="img-fluid" alt="">
+                        <img src="./assets/images/icons/call.png" class="img-fluid" alt="">
                     </div>
 
                 </a>
